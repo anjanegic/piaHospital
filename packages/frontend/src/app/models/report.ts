7b94bcad706a3b340ab77cc3b242b7344448e30c
Miroslav <mstefa@HOTMAIL.COM>
2023-08-21 03:18:02 +0200

    feat(patient): add appointment tab

---
+++ packages/frontend/src/app/models/report.ts
@@ -0,0 +1,10 @@
+export class Report {
+  date: Date;
+  doctorName: string;
+  doctorSpecialization: string;
+  reasonForVisit: string;
+  diagnosis: string;
+  recommendedTherapy: string;
+  recommendedNextAppointment: Date;
+  patient: string;
+}
